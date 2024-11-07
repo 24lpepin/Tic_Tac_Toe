fe0bb4d12fa91c563c3c9776813cd9d2eff5b6a9
Luke Pepin <lpepin1123@gmail.com>
2024-11-07 09:35:35 -0500

    Started implementation of memoization

--- move_finder.py
+++ move_finder.py
@@ -6,13 +6,18 @@ def find_random_move(gs):
     if not valid_moves:
         return None  # Return None if no valid moves are available
     return valid_moves[random.randint(0, len(valid_moves) - 1)]
-
-def find_move_minimax_move_ordering(gs, depth, turn: Literal[1, -1], alpha, beta, max_depth=float('inf')):
-    global counter
+    
+def find_move_minimax_memoization(gs, depth, turn: Literal[1, -1], alpha, beta, memo, max_depth=float('inf')):
+    global counter, move_score_log
     counter += 1
 
     if counter % 100000 == 0:
         print(counter)
+
+    key = gs_to_key(gs, turn)
+    if key in memo:
+        print("return", key, memo[key], depth)
+        return memo[key]
     
     if gs.is_game_over() is not None:
         return gs.is_game_over()
@@ -27,24 +32,28 @@ def find_move_minimax_move_ordering(gs, depth, turn: Literal[1, -1], alpha, beta
         max_score = -float('inf')
         for move in valid_moves:
             gs.make_move(move[0], move[1])
-            score = find_move_minimax_move_ordering(gs, depth + 1, turn * -1, alpha, beta, max_depth)
+            score = find_move_minimax_memoization(gs, depth + 1, turn * -1, alpha, beta, memo, max_depth)
             gs.undo_move()
             max_score = max(score, max_score)
             alpha = max(score, alpha)
             if beta <= alpha:
                 break
+        print(key, max_score, depth)
+        memo[key] = max_score
         return max_score
 
     elif turn == -1:  # O to move -> minimizing
         min_score = float('inf')
         for move in valid_moves:
             gs.make_move(move[0], move[1])
-            score = find_move_minimax_move_ordering(gs, depth + 1, turn * -1, alpha, beta, max_depth)
+            score = find_move_minimax_memoization(gs, depth + 1, turn * -1, alpha, beta, memo, max_depth)
             gs.undo_move()
             min_score = min(score, min_score)
             beta = min(score, beta)
             if beta <= alpha:
                 break
+        print(key, min_score, depth)
+        memo[key] = min_score
         return min_score
 
 def order_moves(gs, valid_moves, turn):
@@ -56,45 +65,65 @@ def order_moves(gs, valid_moves, turn):
         move_scores.append((move, score))
         gs.undo_move()
     
-    move_scores.sort(key=lambda x: x[1], reverse=(turn == 1)) #do i need to reverse this? the numbers are negative when turn is O
+    move_scores.sort(key=lambda x: x[1], reverse=(turn == 1)) 
     
     moves = []
     for move, score in move_scores:
         moves.append(move)
     return moves
+
+def gs_to_key(gs, turn):
+    board = gs.get_board()
+    key = ""
+    for row in board:
+        for col in row:
+            key += str(col)
+    key += " "
+    key += str(turn)
+    
+    return key
         
 
 def find_best_move(gs, max_depth=float('inf')):
-    global counter
+    global counter, move_score_log
+    move_score_log = []
     counter = 0
 
     valid_moves = gs.get_valid_moves()  # Recalculate valid moves after each board change
     turn = gs.turn
     list_of_moves = []
+    memo = {}
 
     if turn == 1:  # X to move -> maximizing
         max_score = -float('inf')
         for move in valid_moves:
+            print("1", move)
             gs.make_move(move[0], move[1])
-            score = find_move_minimax_move_ordering(gs, 0, turn * -1, -float('inf'), float('inf'), max_depth)
+            score = find_move_minimax_memoization(gs, 0, turn * -1, -float('inf'), float('inf'), memo, max_depth)
             gs.undo_move()
             if score > max_score:
                 max_score = score
                 list_of_moves = [move]
+                move_score_log = [(move, score)]
             elif score == max_score:
                 list_of_moves.append(move)
+                move_score_log.append((move, score))
 
     elif turn == -1:  # O to move -> minimizing
         min_score = float('inf')
         for move in valid_moves:
+            print("-1", move)
             gs.make_move(move[0], move[1])
-            score = find_move_minimax_move_ordering(gs, 0, turn * -1, -float('inf'), float('inf'), max_depth)
+            score = find_move_minimax_memoization(gs, 0, turn * -1, -float('inf'), float('inf'), memo, max_depth)
             gs.undo_move()
             if score < min_score:
                 min_score = score
                 list_of_moves = [move]
+                move_score_log = [(move, score)]
             elif score == min_score:
                 list_of_moves.append(move)
-    
-    print(list_of_moves, counter)
+                move_score_log.append((move, score))
+
+    print(counter)   
+    print(move_score_log)
     return list_of_moves
